b82a7d62e22878a27f3319c5d2d012a0622ab18c
Eitan Suez <eitan.suez@gmail.com>
2016-01-14 14:36:47 -0600

    might as well configure beans in Application..

--- src/main/groovy/com/eitan/ContactApplication.groovy
+++ src/main/groovy/com/eitan/ContactApplication.groovy
@@ -1,12 +1,32 @@
 package com.eitan
 
+import com.gemstone.gemfire.cache.client.ClientCache
+import com.gemstone.gemfire.pdx.ReflectionBasedAutoSerializer
+import io.pivotal.spring.cloud.service.gemfire.GemfireServiceConnectorConfig
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.config.java.AbstractCloudConfig
+import org.springframework.cloud.service.ServiceConnectorConfig
+import org.springframework.context.annotation.Bean
 import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories
 
 @SpringBootApplication
 @EnableGemfireRepositories(basePackages = 'com.eitan')
-class ContactApplication {
+class ContactApplication extends AbstractCloudConfig {
+
+  public ServiceConnectorConfig createGemfireConnectorConfig() {
+    GemfireServiceConnectorConfig gemfireConfig = new GemfireServiceConnectorConfig();
+    gemfireConfig.setPdxSerializer(new ReflectionBasedAutoSerializer("com.eitan.Contact"))
+    gemfireConfig
+  }
+
+  @Bean
+  public ClientCache myClientCache() {
+    ClientCache cache = cloud().getServiceConnector("my-gemfire-service",
+        ClientCache.class,
+        createGemfireConnectorConfig())
+    cache
+  }
 
   static void main(String[] args) {
     SpringApplication.run(ContactApplication, args)
