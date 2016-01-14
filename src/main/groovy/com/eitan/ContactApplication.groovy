d09ac3aba89e055db21d22f869df747ff5fb233a
Eitan Suez <eitan.suez@gmail.com>
2016-01-14 14:36:07 -0600

    unused import

--- src/main/groovy/com/eitan/ContactApplication.groovy
+++ src/main/groovy/com/eitan/ContactApplication.groovy
@@ -2,7 +2,6 @@ package com.eitan
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.ImportResource
 import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories
 
 @SpringBootApplication
