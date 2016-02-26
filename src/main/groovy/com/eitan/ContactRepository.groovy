f73eb96a7c3dd972b176d1ee3592a8f04630b6b9
Eitan Suez <eitan.suez@gmail.com>
2016-02-25 22:41:16 -0500

    some finder methods

--- src/main/groovy/com/eitan/ContactRepository.groovy
+++ src/main/groovy/com/eitan/ContactRepository.groovy
@@ -1,8 +1,12 @@
 package com.eitan
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Component
 
 @Component
 interface ContactRepository extends CrudRepository<Contact, Long> {
+  List<Contact> findByAgeGreaterThan(@Param("age") int age)
+  List<Contact> findByAgeLessThan(@Param("age") int age)
+  List<Contact> findByAge(@Param("age") int age)
 }
